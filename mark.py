b1babe2384991bdd8ac4fa465753f8e666acb879
ananpabbas <ananpa2323@gmail.com>
2024-10-01 02:49:32 -0400

    Add files via upload

---
+++ mark.py
@@ -0,0 +1,10 @@
+s1=int(input("enter the mark of english out of 50"))
+s2=int(input("enter the mark of  malayalam out of 50"))
+s3=int(input("enter the mark of hindi out of 50"))
+s4=int(input("enter the mark of maths out of 50"))
+s5=int(input("renter the mark of  science out of 50"))
+mark=int(input("enter the mark of all subject"))
+total=s1+s2+s3+s4+s5
+print("the total mark obtained",total)
+percentage=(total/mark)*100
+print("the percentage is",percentage)
