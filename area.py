d602827774b7305ac4992d76bf757f1abe1e4429
ananpabbas <ananpa2323@gmail.com>
2024-10-01 02:54:55 -0400

    Add files via upload

---
+++ area.py
@@ -0,0 +1,16 @@
+r=int(input("enter the radius"))
+area=3.14*r*r
+print("area of circle:",area)
+
+"""area of triangle"""
+b=float(input("enter breadth"))
+h=float(input("enter height"))
+triangle=0.5*b*h
+print("area of triangle is:",triangle)
+
+
+###area of rectangle###
+l=int(input("enter length"))
+b=int(input("enter breadth"))
+rectangle=l*b
+print("area of rectangle:",rectangle)
