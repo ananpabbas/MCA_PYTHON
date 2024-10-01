a3faacfe4ac02236f89a8dacd380312554d4b962
ananpabbas <ananpa2323@gmail.com>
2024-10-01 02:54:24 -0400

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,10 @@
+x=int(input("enter first number"))
+y=int(input("enter second number"))
+sum=x+y
+print("the sum is:",sum)
+difference=x-y
+print("the difference is:",difference)
+multiplication=x*y
+print("the product is:",multiplication)
+div=x/y
+print("the division is:",div)
