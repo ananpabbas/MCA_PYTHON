10ae3f0353598f38f00a57841d70366ab302ced0
ananpabbas <ananpa2323@gmail.com>
2024-10-09 01:40:42 -0400

    9-10-24

---
+++ week3/largest.py
@@ -0,0 +1,11 @@
+n1=int(input("enter first no"))
+n2=int(input("enter second no:"))
+n3=int(input("enter third no:"))
+if(n1>n2 and n1>n3):
+    print("the largest number is ",n1);
+elif(n2>n1 and n2>n3):
+    print("the largest number is",n2);
+else:
+    print("the largest number is",n3);
+
+    
---
+++ week3/vowel.py
@@ -0,0 +1,48 @@
+string="hello world"
+vowels=['a','e','i','o','u']
+r=""
+for char in string:
+    if char in vowels:
+        print(char)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
